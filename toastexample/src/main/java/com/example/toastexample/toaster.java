e53fe0c54c37a93261b4e2d3b288d590c087aac5
Shraddha Kudale <shraddha@rareworks.io>
2025-03-13 13:24:04 +0530

    Updated test

--- toastexample/src/main/java/com/example/toastexample/toaster.java
+++ toastexample/src/main/java/com/example/toastexample/toaster.java
@@ -7,6 +7,6 @@ import android.widget.Toast;
 public class toaster {
 
     public static void simpleToast(Context context, String msg){
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show(); // TOAST MESSAGE
     }
 }
